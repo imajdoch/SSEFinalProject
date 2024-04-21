9ac9519c0d0a67f2cdb4b06b6e436c0e444ad5a9
Elise Thibodeau <117480206+ethibodeau0825@users.noreply.github.com>
2024-04-21 19:54:20 -0400

    Update addbook.js
    me fixing my silly little error

--- addbook.js
+++ addbook.js
@@ -4,13 +4,13 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, on
 
 // Initialize Firebase
 var firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
+    apiKey: "AIzaSyDLhWz4KLKNNuDrNhhtLY8Xrzy75RZ7LTE",
+    authDomain: "ssefinalproject.firebaseapp.com",
+    projectId: "ssefinalproject",
+    storageBucket: "ssefinalproject.appspot.com",
+    messagingSenderId: "706635744855",
+    appId: "1:706635744855:web:c6a8a54603d38269a60512",
+    measurementId: "G-FEWQLQ3PH8"
 };
 
 const t = firebase.initializeApp(firebaseConfig);
