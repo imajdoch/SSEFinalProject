ee15c588988f72a6126096b3045196f0ce2c8e07
Elise Thibodeau <117480206+ethibodeau0825@users.noreply.github.com>
2024-04-21 18:07:05 -0400

    Update addbook.js

--- addbook.js
+++ addbook.js
@@ -1,6 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { setDoc, doc, getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
+var firebaseConfig = {
+    apiKey: "AIzaSyDLhWz4KLKNNuDrNhhtLY8Xrzy75RZ7LTE",
+    authDomain: "ssefinalproject.firebaseapp.com",
+    projectId: "ssefinalproject",
+    storageBucket: "ssefinalproject.appspot.com",
+    messagingSenderId: "706635744855",
+    appId: "1:706635744855:web:c6a8a54603d38269a60512",
+    measurementId: "G-FEWQLQ3PH8"
+};
 // JavaScript for handling image upload and book addition
 document.getElementById('addBook').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission behavior
